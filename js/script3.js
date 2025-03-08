fcd10c8fde67d8a9ad89c37aede18a4967e6182e
Augustus Production <130487763+AugusteY13@users.noreply.github.com>
2025-03-09 00:05:13 +0100

    Create script3.js

---
+++ js/script3.js
@@ -0,0 +1,20 @@
+function rediriger() {
+    let valeur = document.getElementById("userInput").value.toLowerCase();
+    
+    // Liste des pages associées aux entrées
+    let pages = {
+        "dragon" : "interactive dragon.html",
+        "smiley" : "smiley.html",
+        "seche" : "seche.html",
+        "aetheria" : "https://docs.google.com/document/d/1QtS9tzhbet8Yr-0J8mPBJFF-uzzJFUpUzwMjKH1GnKk/edit?usp=sharing",
+        "hibiscus" : "https://docs.google.com/document/d/1F7p4HRatPLGH74AcphkRJdD2rU2XL5ulMONGL43dqRo/edit?usp=sharing",
+        "fakes" : "https://docs.google.com/document/d/1Gr0qLRNe_ZuQ4i6itQ5sHU2DDsX_0u1iEa0lzM6hSbw/edit?usp=sharing",
+        "roman" : "https://sites.google.com/view/lesechosduplaisir",
+    };
+
+    if (pages[valeur]) {
+        window.open(pages[valeur], "_blank");
+    } else {
+        alert("Page non trouvée !");
+    }
+}
