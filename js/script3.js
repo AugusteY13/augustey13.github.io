826f5fe71bf06ac40f9d44ae6d0901a155d3cdba
Augustus Production <130487763+AugusteY13@users.noreply.github.com>
2025-03-15 19:22:43 +0100

    Update script3.js

--- js/script3.js
+++ js/script3.js
@@ -8,6 +8,7 @@ function rediriger() {
         "seche" : "seche.html",
         "aetheria" : "https://docs.google.com/document/d/1QtS9tzhbet8Yr-0J8mPBJFF-uzzJFUpUzwMjKH1GnKk/edit?usp=sharing",
         "hibiscus" : "https://docs.google.com/document/d/1F7p4HRatPLGH74AcphkRJdD2rU2XL5ulMONGL43dqRo/edit?usp=sharing",
+        "casino" : "https://docs.google.com/document/d/16bmJEaa0GNEs8iXPI0gsoUrysb_4WH0mmymG_c4KAEk/edit?usp=sharing",
         "fakes" : "https://docs.google.com/document/d/1Gr0qLRNe_ZuQ4i6itQ5sHU2DDsX_0u1iEa0lzM6hSbw/edit?usp=sharing",
         "roman" : "https://sites.google.com/view/lesechosduplaisir",
     };
