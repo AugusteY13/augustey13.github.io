ab973110c8158565b57e882ffc084036f89b7012
Augustus Production <130487763+AugusteY13@users.noreply.github.com>
2026-08-21 21:14:27 +0900

    Change Discord webhook URL in contact form
    Updated Discord webhook URL in form submission handler.

--- js/form2discord.js
+++ js/form2discord.js
@@ -1,7 +1,7 @@
 document.getElementById("contactForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
-    const webhookURL = "https://discord.com/api/webhooks/1498267504194097262/5d5brNUFAf6vkn-dDrXni-tP5ujuIAqiAdkIEHUodQmN3ruAOnYBCk2GcjKm3kifHnab";
+    const webhookURL = "https://discord.com/api/webhooks/1540332778787307581/_SzzqPJxsA0BIYehHjRp8OtyKTsxfiYRXzigSbKGp6feH9lJXzv_CYufUM4LQa8gyBRA";
 
     const formData = new FormData(e.target);
 
